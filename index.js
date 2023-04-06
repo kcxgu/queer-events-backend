fcc0ca1e1f3d08d18b2960e4b66817ce6921cbc6
Kaycee Gu <kayceegu@Kaycees-MacBook-Air.local>
2023-04-06 14:11:08 +0100

    initiated set up, added post form logic

---
+++ index.js
@@ -0,0 +1,39 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import { eventsRouter } from "./routes/events.js";
+
+const app = express();
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+mongoose.set('strictQuery', true);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/events", eventsRouter)
+
+const host = process.env.HOST;
+const port = process.env.PORT;
+
+const db = async () => {
+    console.log(`Connecting to database at: ${process.env.DB_URI}`)
+
+    try {
+        await mongoose.connect(process.env.DB_URI);
+        console.log(`Database connected at: ${process.env.DB_URI}`);
+    } catch (error) {
+        console.log(`Database error: ${error.message}`)
+    }
+}
+
+db();
+
+const server = app.listen(port, host, () => {
+    const SERVERHOST = server.address().address;
+    const SERVERPORT = server.address().port;
+    console.log(`Server is running on http://${SERVERHOST}:${SERVERPORT}`)
+})
